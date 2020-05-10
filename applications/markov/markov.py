91dc815b814b594875581deac4dd0521aaeba3c8
cmruss <cmrussell08@gmail.com>
2020-05-10 03:12:12 -0400

    refactored code

--- applications/markov/markov.py
+++ applications/markov/markov.py
@@ -7,32 +7,39 @@ with open("input.txt") as f:
 
 # TODO: analyze which words can follow other words
 def markov(s):
+    # our dictionary
     cache = {}
+    # makes the string a list, removing extra spaces
     s = " ".join(s.split()).split(" ")
+    # initialize prev_word for loop
     prev_word = ""
     # loop through the new list
     for word in s:
+        # remove characters
         word = word.strip('()"}{')
         # if the word's in the cache
         if cache.get(f"{word}"):
-            #add it to the prvious word's value
-            cache[prev_word].append(word)
-        # if the word is not in the cache and the previous word has been set
-        elif not cache.get(f"{word}") and len(prev_word) > 0:
+            # just add it to the prvious word's value
+            cache[prev_word].append(word) 
+        else: # if the word is not in the cache
             # enter current word into cache with empty list as value
             cache[word] = []
-            # append the current word to the previous word's list
-            cache[prev_word].append(word)
-        else:
-            # base case baby
-            cache[word] = []
+            # if the previous word has been set
+            if len(prev_word) > 0:
+                # append the current word to the previous word's list
+                cache[prev_word].append(word)
         # set this word to the previous word
         prev_word = word
     # print(cache)
+    # make the dict keys a list so we can randomly iterate
     keys = list(cache.keys())
+    # sort the list randomly
     random.shuffle(keys)
+    # intialize sentence as a list of words 
     sentence = []
+    # initialize word count
     word_count = 0
+    # initialize new_word for next word
     new_word = ""
     #loop through the keys
     for word in keys: # until we find a starter word
@@ -41,25 +48,26 @@ def markov(s):
             sentence.append(f"{word}")
             # increment word_count
             word_count += 1
+            # make a word in the cache list the new word
             new_word = random.choice(cache[word])
+            # exit loop
             break
     # loop until the next word has no values
     while cache[new_word] != []:
             # pick a random word from the value                
-            new_word = random.choice(list(cache[new_word]))
+            new_word = random.choice(cache[new_word])
             # add it to the sentence
             sentence.append(f"{new_word}")
             word_count += 1
+            # if the last character in the added word is one of these..
             if new_word[-1] in list(["." ,"?" ,"!"]):
-                print(" ".join(sentence))
-                # print(cache['to'])
-                break
-    
+                return (" ".join(sentence), word_count)
 
 # TODO: construct 5 random sentences
 
-markov(words)
-markov(words)
-markov(words)
-markov(words)
-markov(words)
+print(markov(words))
+print(markov(words))
+print(markov(words))
+print(markov(words))
+print(markov(words))
+print(markov(words))
